4334f182b08c0cbf4eb77c26fcd576331b472a98
Victor Oliveira <victor.oliveira@blueservice.com.br>
2023-04-14 13:51:02 -0300

    retirando dados do home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -8,36 +8,10 @@ import { format, parseISO } from 'date-fns';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  @ViewChild(IonDatetime) datetime: IonDatetime;
-  dateValue = format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z';
-  formattedString: any = '';
 
   constructor() {  }
   ngOnInit(): void {
-    this.setToday();
   }
 
-  setToday() {
-    const hoje = new Date();
-    const dia = hoje.getDate().toString().padStart(2, '0');
-    const mes = String(hoje.getMonth() + 1).padStart(2, '0');
-    const ano = hoje.getFullYear();
-    const hora = hoje.getHours();
-    const minutos = hoje.getMinutes();
-    const dataAtual = `${dia}/${mes}/${ano}  ${hora}:${minutos}`;
-    this.formattedString = dataAtual;
-  }
 
-  modalDateChanged(value) {
-    this.dateValue = value;
-    this.formattedString = format(parseISO(value), 'dd/MM/yyyy HH:mm');
-  }
-
-  async close() {
-    await this.datetime.cancel(true);
-  }
-
-  async select() {
-    await this.datetime.confirm(true);
-  }
 }
